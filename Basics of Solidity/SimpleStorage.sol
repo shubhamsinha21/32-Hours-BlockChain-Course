893fa20b4c3e68c82f826d3f911c759c8f2562ef
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2023-06-27 18:04:25 +0530

    Completed Storage.sol | Lesson 1&2

--- Basics of Solidity/SimpleStorage.sol
+++ Basics of Solidity/SimpleStorage.sol
@@ -1,33 +1,43 @@
-// SPDX-License-Identifier: MIT
-pragma solidity  ^0.8.18;
+// SPDX-License-Identifier:MIT
+pragma solidity 0.8.19; //solidity version
 
-contract SimpleStorage{
+contract SimpleStorage {
+// favoriteNumber initialized to 0 if no value is given
+// defaulted to internal favoriteNumber
+   uint public favoriteNumber; //0
 
-    //boolean, uint, int, address, bytes
-    // bool hasFavoriteNumber = true;
-    // string favoriteNumberInText = 'five';
-    // int256 favoriteInt = -5;
-    // address myAddress = 0xC5Bd9A9e4032079206F2fBBe6B73b2155B31B67B;
-    // bytes32 favoriteBytes = 'cat';
-    // - this is initialized to 0
+// list of favoriteNumbers (array) [0,34,56]
+uint256[] listOfFavoriteNumbers; 
 
-    uint256 public favoriteNumber ;
+//structs
+struct Person{
+    uint256 myFavoriteNumber;
+    string name;
+}
+// being explicit and define 1 by 1
+// Person public myFriend = Person({myFavoriteNumber:5, name: "Pat"});
+// Person public myFriend2 = Person({myFavoriteNumber:6, name: "Peter"});
 
-     function store(uint256 _favoriteNumber) public  {
-        favoriteNumber =  _favoriteNumber;
-        
-    }
+// Rather calling one by one, we shall consider an array
+Person[] public listOfPeople; //[] - default is empty list
 
-    // view, pure - we can only view - no modeification of state & no reading from blockchain state
+// mapping , chelsi -> 234
+mapping (string => uint256) public nametofavoriteNumber;
 
-    function retrieve() public view returns(uint256) {
+
+// store function for assigning value t0 a var _favoriteNumber
+   function store(uint256 _favoriteNumber) public {
+       favoriteNumber = _favoriteNumber;
+   }
+// view, pure
+    function retrieve() public view returns (uint256){
         return favoriteNumber;
     }
 
-    function add() public pure returns(uint256){
-    return (1+1);
-    
+// creating a function that will allow us to add & update array
+// calldata, memory & storage
+    function addPerson(string memory _name, uint256 _myFavoriteNumber) public {
+        listOfPeople.push( Person(_myFavoriteNumber, _name) );
+        nametofavoriteNumber[_name] = _myFavoriteNumber;
     }
-
-
 }
